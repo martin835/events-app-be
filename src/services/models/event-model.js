6f1cc4a05b7318eb9cb4b861abcc21350671b177
martin835 <64438132+martin835@users.noreply.github.com>
2022-08-10 22:02:33 +0200

    function that pushes scraped info to DB works

--- src/services/models/event-model.js
+++ src/services/models/event-model.js
@@ -5,16 +5,17 @@ const { Schema, model } = mongoose;
 const EventSchema = new Schema(
   {
     title: { type: String, required: true },
-    description: { type: String, required: true },
+    //description: { type: String, required: true },
     categories: [String],
-    // startDate: { type: Date, required: true },
-    // endDate: { type: Date, required: true },
+    // startDate: { type: String, required: true },
+    // endDate: { type: String, required: true },
     // month: { type: Number, required: true },
     // location: {
     //   country: { type: String, required: true },
     //   city: { type: String, required: true },
     //   region: { type: String, required: true },
     // },
+    time: { type: String },
     webpage: { type: String },
     price: { type: Number },
     familyFriendly: { type: Boolean },
--- src/tools/eventScraper.js
+++ src/tools/eventScraper.js
@@ -1,30 +1,7 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 import OrganizerModel from "../services/models/organizer-model.js";
-
-// const getShows = async () => {
-//   const url = "https://bdnr.sk/";
-//   const organizer = await OrganizerModel.findById("62ea65b94aa5b228b3b93d2c");
-//   const shows = [];
-//   axios(url)
-//     .then((response) => {
-//       const html = response.data;
-//       const $ = cheerio.load(html);
-
-//       $(`.program-block`, html).each(function () {
-//         const date = $(this).find(".datumX").text();
-//         const time = $(this).find(".casX").text();
-//         const title = $(this).find(".program-obsah").find("h2").text();
-
-//         shows.push({
-//           date,
-//           time,
-//           title,
-//         });
-//       });
-//     })
-//     .catch((err) => console.log(err))
-// };
+import EventModel from "../services/models/event-model.js";
 
 const scrapeShows = async () => {
   const shows = [];
@@ -46,6 +23,20 @@ const scrapeShows = async () => {
           title,
         });
       });
+
+      //This function saves scraped shows into the DB
+
+      // shows.forEach(async (show) => {
+      //   const newEvent = new EventModel({
+      //     title: show.title,
+      //     // categories: ["theater"],
+      //     // startDate: show.date,
+      //     // endDate: show.date,
+      //     // time: show.time,
+      //   });
+      //   const { _id } = await newEvent.save();
+      // });
+
       console.log(organizer);
       console.log(shows);
     } else {
